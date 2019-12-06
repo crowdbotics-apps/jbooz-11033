1e9a3156bfd938ef95b7d555ed276d4280ffaa25
Crowdbotics <team@crowdbotics.com>
2019-12-06 04:37:18 +0000

    Initial commit.

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: waitress-serve --port=$PORT backend.jbooz_11033.wsgi:application
+release: ./backend/release-tasks.sh
---
+++ backend/jbooz_11033/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jbooz_11033 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jbooz_11033.settings")
+
+application = get_wsgi_application()
